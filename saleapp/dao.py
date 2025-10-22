91392561af210f48d108875738c061516f40f556
Thuy Ngan <bntngan1305@gmail.com>
2025-10-22 11:00:48 +0700

    BTH1

---
+++ saleapp/dao.py
@@ -0,0 +1,20 @@
+import json
+
+def load_categories():
+    with open("data/category.json", encoding="utf-8") as f:
+        return json.load(f)
+
+def load_products(q=None, cate_id=None):
+    with open("data/product.json", encoding="utf-8") as f:
+        products=json.load(f)
+
+        if q:
+            products = [p for p in products if p["name"].find(q)>=0]
+
+        if cate_id:
+            products = [p for p in products if p["cate_id"].__eq__(int(cate_id))]
+
+        return products
+
+if __name__=="main":
+    print(load_categories())
